6696fe71b735ea59f9c5108b6d2c639e3f4239df
Dan Stetsenko <oddestdan@gmail.com>
2021-03-13 17:58:01 +0200

    Project completed (I think...)

--- js/game.js
+++ js/game.js
@@ -31,20 +31,59 @@ const game = () => {
         const randomNumber0to2 = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[randomNumber0to2];
 
-        // сравниваем ручки
-        compareHands(this.textContent, computerChoice);
+        setTimeout(() => {
+          // сравниваем ручки
+          compareHands(this.textContent, computerChoice);
 
-        // поменять фотки для рук человека и компьютера
-        playerHand.src = `assets/${this.textContent}.png`;
-        computerHand.src = `assets/${computerChoice}.png`;
+          // поменять фотки для рук человека и компьютера
+          playerHand.src = `assets/${this.textContent}.png`;
+          computerHand.src = `assets/${computerChoice}.png`;
+        }, 2000);
+
         // проиграть анимацию shake
+        playerHand.style.animation = 'shakePlayer 2s ease';
+        computerHand.style.animation = 'shakeComputer 2s ease';
       });
     });
   };
 
-  const updateScore = () => {};
+  const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
 
-  const compareHands = (playerChoice, computerChoice) => {};
+  const compareHands = (playerChoice, computerChoice) => {
+    const result = document.querySelector('.result-text');
+
+    const lowercasePlayerChoice = playerChoice.toLowerCase();
+    const lowercaseComputerChoice = computerChoice.toLowerCase();
+
+    if (lowercasePlayerChoice === lowercaseComputerChoice) {
+      result.textContent = 'Ничья';
+      return;
+    }
+
+    function setupVictoryOption(playerOption, computerOption) {
+      if (lowercasePlayerChoice === playerOption) {
+        if (lowercaseComputerChoice === computerOption) {
+          result.textContent = 'Игрок выиграл!';
+          pScore++;
+        } else {
+          result.textContent = 'Компьютер выиграл!';
+          cScore++;
+        }
+        updateScore();
+        return;
+      }
+    }
+
+    // Настройка наших условий для победы
+    setupVictoryOption('rock', 'scissors');
+    setupVictoryOption('paper', 'rock');
+    setupVictoryOption('scissors', 'paper');
+  };
 
   startGame();
   playMatch();
